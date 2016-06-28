ff950582027f02697275b7a8ce3d2cf842f2ef97
Max Truxa <dev@maxtruxa.com>
2016-06-29 01:19:31 +0200

    Replace var with let in index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -13,43 +13,43 @@ describe('UserError', function() {
   describe('constructor', function() {
 
     it('should be an instance of UserError', function() {
-      var err = new UserError();
+      let err = new UserError();
       expect(err).to.be.a(UserError);
     });
 
     it('should be an instance of Error', function() {
-      var err = new UserError();
+      let err = new UserError();
       expect(err).to.be.an(Error);
     });
 
     it('should set name to "UserError"', function() {
-      var err = new UserError();
+      let err = new UserError();
       expect(err).to.have.property('name', 'UserError');
     });
 
     it('should set message to null if none is given', function() {
-      var err = new UserError();
+      let err = new UserError();
       expect(err).to.have.property('message', null);
     });
 
     it('should set message', function() {
-      var err = new UserError('foo bar');
+      let err = new UserError('foo bar');
       expect(err).to.have.property('message', 'foo bar');
     });
 
     it('should set additional properties', function() {
-      var err = new UserError(null, {foo: 'bar'});
+      let err = new UserError(null, {foo: 'bar'});
       expect(err).to.have.property('foo', 'bar');
     });
 
     it('should accept properties as first argument', function() {
-      var err = new UserError({foo: 'bar'});
+      let err = new UserError({foo: 'bar'});
       expect(err).to.have.property('message', null);
       expect(err).to.have.property('foo', 'bar');
     });
 
     it('should set name from properties', function() {
-      var err = new UserError({name: 'FooError'});
+      let err = new UserError({name: 'FooError'});
       expect(err).to.have.property('name', 'FooError');
     });
 
@@ -63,17 +63,17 @@ describe('UserError', function() {
     });
 
     it('should set message from properties', function() {
-      var err = new UserError({message: 'foo'});
+      let err = new UserError({message: 'foo'});
       expect(err).to.have.property('message', 'foo');
     });
 
     it('should prefer explicit message over properties', function() {
-      var err = new UserError('foo', {message: 'bar'});
+      let err = new UserError('foo', {message: 'bar'});
       expect(err).to.have.property('message', 'foo');
     });
 
     it('should set stack', function() {
-      var err = new UserError('foo');
+      let err = new UserError('foo');
       expect(err).to.have.property('stack');
       let stack = err.stack.split(/\n\s*/);
       expect(stack[0]).to.equal('UserError: foo');
@@ -82,7 +82,7 @@ describe('UserError', function() {
 
     it('should set stack from properties', function() {
       let stack = {};
-      var err = new UserError({stack});
+      let err = new UserError({stack});
       expect(err).to.have.property('stack', stack);
     });
 
@@ -91,12 +91,12 @@ describe('UserError', function() {
   describe('toString()', function() {
 
     it('should return correctly formatted string for null message', function() {
-      var err = new UserError();
+      let err = new UserError();
       expect(err.toString()).to.equal('UserError: null');
     });
 
     it('should return correctly formatted string', function() {
-      var err = new UserError('test');
+      let err = new UserError('test');
       expect(err.toString()).to.equal('UserError: test');
     });
 
